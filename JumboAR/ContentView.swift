16ef70201cbd0855627823c7c236590c085d650d
Kristiyan Strahilov <55084258+kpuc00@users.noreply.github.com>
2022-09-29 19:42:30 +0200

    Shopping list

--- JumboAR/ContentView.swift
+++ JumboAR/ContentView.swift
@@ -8,29 +8,93 @@
 import SwiftUI
 import RealityKit
 
+let arView = ARView(frame: .zero)
+
 struct ContentView : View {
+//    @StateObject var globalString = GlobalString()
+    @State private var selectedProduct = "Juice"
+    
     var body: some View {
-        ARViewContainer().edgesIgnoringSafeArea(.all)
+        ZStack {
+            ARViewContainer(selectedProduct: selectedProduct).edgesIgnoringSafeArea(.all)
+            HStack {
+                Spacer()
+                VStack {
+                    Menu {
+                        Button("Milk"){ shoppingListBtnAction(str: "Milk")}
+                        Button("Juice"){ shoppingListBtnAction(str: "Juice")}
+                    } label: {
+                        Label("Shopping list", systemImage: "cart").padding(10)
+                    }.buttonStyle(.bordered)
+                    Text(selectedProduct)
+                    
+                    Spacer()
+                    
+                    Button {
+                        searchProductsBtnAction()
+                    } label:{
+                        Label("Search products", systemImage: "location.magnifyingglass").padding(10)
+                    }.buttonStyle(.bordered)
+                }
+            }.padding(10)
+        }
+    }
+    
+    func shoppingListBtnAction(str: String) {
+        selectedProduct = str
+    }
+    
+    func searchProductsBtnAction() {
+        print("Search products button clicked")
     }
 }
 
+//class GlobalString: ObservableObject {
+//  @Published var selectedProduct = ""
+//}
+
 struct ARViewContainer: UIViewRepresentable {
+    var selectedProduct: String
     
     func makeUIView(context: Context) -> ARView {
-        
-        let arView = ARView(frame: .zero)
-        
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
+        let milkAnchor = try! Experience.loadMilk()
+        let juiceAnchor = try! Experience.loadJuice()
+        let testAnchor = try! Experience.loadTestScene()
+        print("arview \(selectedProduct)")
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        if(selectedProduct=="Milk"){
+            print("Showing milk")
+            arView.scene.anchors.removeAll()
+            arView.scene.anchors.append(milkAnchor)
+        }
+        if(selectedProduct=="Juice"){
+            print("Showing juice")
+            arView.scene.anchors.removeAll()
+            arView.scene.anchors.append(juiceAnchor)
+        }
+        
+        arView.scene.anchors.append(testAnchor)
         
         return arView
         
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    func updateUIView(_ uiView: ARView, context: Context) {
+        print("kur",selectedProduct)
+        let milkAnchor = try! Experience.loadMilk()
+        let juiceAnchor = try! Experience.loadJuice()
+        if(selectedProduct=="Milk"){
+            print("Showing milk")
+            arView.scene.anchors.removeAll()
+            arView.scene.anchors.append(milkAnchor)
+        }
+        if(selectedProduct=="Juice"){
+            print("Showing juice")
+            arView.scene.anchors.removeAll()
+            arView.scene.anchors.append(juiceAnchor)
+        }
+    }
     
 }
 
